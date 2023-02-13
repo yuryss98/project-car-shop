82b5cb396b0b3dfd87754bb5d24bd5cd8563990f
Yury Stolarz <yurysscareer@gmail.com>
2023-02-13 17:03:47 -0300

    feat: endpoint POST /cars

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use('/cars', routes.CarRoutes);
 
 export default app;
---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+import CarRoutes from './car.routes';
+
+export default {
+  CarRoutes,
+};
