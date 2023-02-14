b9ffed567fd57a8c526d9aab1efcbd82982cec75
Yury Stolarz <yurysscareer@gmail.com>
2023-02-14 17:17:01 -0300

    feat: add entity Motorcycle

---
+++ src/Domains/Motorcycle.ts
@@ -0,0 +1,14 @@
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import Vehicle from './Vehicle';
+
+export default class Motorcycle extends Vehicle {
+  private category: 'Street' | 'Custom' | 'Trail';
+  private engineCapacity: number;
+
+  constructor(motorcycle: IMotorcycle) {
+    super(motorcycle);
+
+    this.category = motorcycle.category;
+    this.engineCapacity = motorcycle.engineCapacity;
+  }
+}
