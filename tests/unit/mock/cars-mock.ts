ca850eb83170bccd76a5a5598c82c99660c497ab
Yury Stolarz <yurysscareer@gmail.com>
2023-02-13 21:10:35 -0300

    test: add mock of cars

---
+++ tests/unit/mock/cars-mock.ts
@@ -0,0 +1,36 @@
+const cars = [
+  {
+    id: '6348513f34c397abcad040b2',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15990,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+
+  {
+    id: '6348513f34c397abcad040b3',
+    model: 'Ferrari',
+    year: 2023,
+    color: 'Red',
+    status: true,
+    buyValue: 199999999,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+
+  {
+    id: '6348513f34c397abcad040b4',
+    model: 'Siena',
+    year: 2020,
+    color: 'Blue',
+    status: true,
+    buyValue: 75990,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+];
+
+export default cars;
