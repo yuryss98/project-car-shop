9f3eabe274fe747e01637c176d3a376cacab99ba
Yury Stolarz <yurysscareer@gmail.com>
2023-02-14 17:18:00 -0300

    feat: MotorCycle service

---
+++ src/Services/MotorcycleService.ts
@@ -0,0 +1,43 @@
+import Motorcycle from '../Domains/Motorcycle';
+import HttpException from '../error/HttpException';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import MotorcycleODM from '../Models/MotorcycleODM';
+
+export default class MotorcycleService {
+  private ODM: MotorcycleODM;
+
+  constructor() {
+    this.ODM = new MotorcycleODM();    
+  }
+
+  async create(motorCycle: IMotorcycle) {
+    const newMotorcycle = await this.ODM.create(motorCycle);
+    return new Motorcycle(newMotorcycle);
+  }
+
+  async getAll() {
+    const motorcycles = await this.ODM.findAll() as IMotorcycle[];
+
+    return motorcycles.map((motorcycle) => new Motorcycle(motorcycle));
+  }
+
+  async getById(id: string) {
+    const motorcycle = await this.ODM.findById(id) as IMotorcycle;
+
+    if (!motorcycle) {
+      throw new HttpException(404, 'Motorcycle not found');
+    }
+
+    return new Motorcycle(motorcycle);
+  }
+
+  async update(id: string, motorcycle: IMotorcycle) {
+    await this.getById(id);
+
+    const updatedMotorcycle = new Motorcycle({ id, ...motorcycle });
+
+    await this.ODM.update(id, motorcycle);
+
+    return updatedMotorcycle;
+  }
+}
