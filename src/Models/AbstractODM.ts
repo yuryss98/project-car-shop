d0d07ffd5378febd5d68956a495fcb25af057dc3
Yury Stolarz <yurysscareer@gmail.com>
2023-02-14 17:19:36 -0300

    feat: add model generic

---
+++ src/Models/AbstractODM.ts
@@ -0,0 +1,30 @@
+import {
+  Model,
+  Schema,
+  model,
+  models,
+} from 'mongoose';
+
+export default abstract class AbstractODM<T> {
+  private model: Model<T>;
+
+  constructor(private schema: Schema, collection: string) {
+    this.model = models[collection] || model(collection, this.schema);
+  }
+
+  async create(vehicle: T): Promise<T> {
+    return this.model.create({ ...vehicle });
+  }
+
+  async findAll(): Promise<T[]> {
+    return this.model.find({ });
+  }
+
+  async findById(id: string): Promise<T | null> {
+    return this.model.findOne({ _id: id });
+  }
+
+  async update(id: string, updateVehicle: T) {
+    await this.model.updateOne({ id }, { updateVehicle });
+  }
+}
